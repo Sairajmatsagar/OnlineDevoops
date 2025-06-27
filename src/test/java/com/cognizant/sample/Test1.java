11deb2f2b68e81f8083e6effdaa684ff8f5aa4fd
Sairajmatsagar <sairajmatsagar123@gmail.com>
2025-06-27 10:18:15 +0530

    first commit

---
+++ src/test/java/com/cognizant/sample/Test1.java
@@ -0,0 +1,11 @@
+package com.cognizant.sample;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void test1() {
+		System.out.println("Test1");
+	}
+
+}
---
+++ src/test/java/com/cognizant/sample/Test2.java
@@ -0,0 +1,11 @@
+package com.cognizant.sample;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	@Test
+	public void test2() {
+		System.out.println("Test2");
+	}
+
+}
