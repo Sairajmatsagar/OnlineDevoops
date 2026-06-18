891034f10365fbed9e0f5c73d54f6f25bf502a85
Sairajmatsagar <sairajmatsagar123@gmail.com>
2026-06-18 23:30:51 +0530

    this the finnnal one

---
+++ src/test/java/com/cognizant/sample/Test4.java
@@ -0,0 +1,17 @@
+package com.cognizant.sample;
+
+import org.testng.annotations.Test;
+
+public class Test4 {
+	@Test
+	public void test4() {
+		System.out.println("Mein hoon don");
+	}
+
+
+         @Test
+	public void test5() {
+		System.out.println("Fuska don");
+	}
+
+}
