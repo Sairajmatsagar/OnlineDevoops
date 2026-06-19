99e9512bcb5750bcb9e3706d6008629b70ce7535
Sairajmatsagar <sairajmatsagar123@gmail.com>
2026-06-19 11:23:46 +0530

    Yashwant commit

---
+++ src/test/java/com/cognizant/sample/Test5.java
@@ -0,0 +1,17 @@
+package com.cognizant.sample;
+
+import org.testng.annotations.Test;
+
+public class Test5 {
+	@Test
+	public void test4() {
+		System.out.println("Gopal hein don");
+	}
+
+
+         @Test
+	public void test5() {
+		System.out.println("proper don");
+	}
+
+}
