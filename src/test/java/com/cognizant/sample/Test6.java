175f9fe02ca2f50f8faa2f423a18db08a8ae629a
Sairajmatsagar <sairajmatsagar123@gmail.com>
2026-06-19 14:34:24 +0530

    shekhar bhai cimmit

---
+++ src/test/java/com/cognizant/sample/Test6.java
@@ -0,0 +1,17 @@
+package com.cognizant.sample;
+
+import org.testng.annotations.Test;
+
+public class Test5 {
+	@Test
+	public void test4() {
+		System.out.println("Shekhar hein don");
+	}
+
+
+         @Test
+	public void test5() {
+		System.out.println("he is  don of sbd1 7th floor");
+	}
+
+}
