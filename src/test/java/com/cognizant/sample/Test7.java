3bcfad177c0b3eee4d7ba6a567b1db3ce796272a
Sairajmatsagar <sairajmatsagar123@gmail.com>
2026-06-19 14:42:16 +0530

    again trrying

--- src/test/java/com/cognizant/sample/Test7.java
+++ src/test/java/com/cognizant/sample/Test7.java
@@ -2,7 +2,7 @@ package com.cognizant.sample;
 
 import org.testng.annotations.Test;
 
-public class Test5 {
+public class Test7 {
 	@Test
 	public void test4() {
 		System.out.println("Shekhar hein don");
